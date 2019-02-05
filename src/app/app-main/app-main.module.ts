4fb23a0709dc8eb5b8f90c5c35b366ef835e1bbb
jmadrigal@novitec.cr <LaVidaEsBella01$>
2019-02-05 06:25:35 -0600

    Fix Relative path

--- src/app/app-main/app-main.module.ts
+++ src/app/app-main/app-main.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AppMainRoutingModule } from './app-main-routing.module';
-import { AppCommonModule } from 'src/app/app-common/app-common.module';
+import { AppCommonModule } from '../app-common/app-common.module';
 import { MainComponent } from './main/main.component';
 
 @NgModule({
